55df58a69a84a86bbdef2d787369cc9a397ff6c1
AntoanYosifov <134812071+AntoanYosifov@users.noreply.github.com>
2023-05-28 19:02:26 +0200

    Second try of commit.

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class RockPaperScissors {
@@ -5,6 +6,7 @@ public class RockPaperScissors {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
        String playerMove = scanner.nextLine();
+        Random computerMove = new Random(3);
        
     }
 }
